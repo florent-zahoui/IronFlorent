05fc01739c30a9cb0ad8dca25fefeed208eb0fd7
Florent Zahoui <florent.zahoui@gmail.com>
2022-01-24 19:16:36 +0100

    Labs
    Labs

---
+++ Labs/Unit 3/Lab _ SQL Intro/Lab - SQL Intro 1.sql
@@ -0,0 +1,8 @@
+
+select * from sakila.actor;
+select * from sakila.address;
+select title from sakila.film;
+select name as language from sakila.language;
+select * from sakila.store;
+select * from sakila.staff;
+select first_name from sakila.staff;
